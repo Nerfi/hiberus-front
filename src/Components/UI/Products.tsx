13d4dc1ce8a255182819835fcc347fc9dc0f07e4
jfparedes <jfparedes@hiberus.com>
2022-10-03 16:04:08 +0200

    feat: sort by price done

--- src/Components/UI/Products.tsx
+++ src/Components/UI/Products.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Product from "../Product";
 import axios, { AxiosResponse } from "axios";
-import { Flex, Center, Input } from "@chakra-ui/react";
+import { Flex, Center, Input, Box } from "@chakra-ui/react";
 import { Items } from "../Interfaces/ContextType";
 
 const Products: React.FC = () => {
   const [userQuery, setQuery] = useState<string>("");
   const [filteredProducts, setFilteredProducts] = useState<Items[]>([]);
+  const [sortByPrice, setSort] = useState<boolean>(false);
 
   const handleChange = (e: {
     target: { value: React.SetStateAction<string> };
@@ -23,6 +24,7 @@ const Products: React.FC = () => {
           responseType: "stream",
         });
         setFilteredProducts(retrieveData.data);
+        
       } catch (error) {
         console.log(error);
       }
@@ -30,6 +32,22 @@ const Products: React.FC = () => {
 
     filterByUserQuery(userQuery);
   }, [userQuery]);
+
+  const handleSortByPrice = async (): Promise<void> => {
+    try {
+      setSort((prev) => !prev);
+      const sortedResults = await axios({
+        method: "GET", 
+        url: sortByPrice ? "http://localhost:3001/products?_sort=price&_order=asc" : "http://localhost:3001/products?_sort=price&_order=desc",
+        responseType: "stream"
+      })
+
+      setFilteredProducts(sortedResults.data)
+
+      console.log(sortedResults.data, "resuts sorted") //working
+    } catch (error) {}
+  };
+
   return (
     <>
       <Center mt="2rem" m="2rem">
@@ -41,11 +59,15 @@ const Products: React.FC = () => {
           htmlSize={40}
           border="solid #000000"
         />
-        <Input
-          type="number"
-          placeholder="Search by price"
-          border="solid #000000"
-        />
+        <Box border="solid #000000" p="1px" display="flex" gap="1rem">
+          <input
+            type="checkbox"
+            name="sort"
+            id=""
+            onClick={handleSortByPrice}
+          />
+          <label>Sort by price</label>
+        </Box>
       </Center>
 
       <Flex justify="center" m="6em" gap="10%" wrap="wrap" h="60vh">
